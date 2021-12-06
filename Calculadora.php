5f872765cff53cefb8a42058f078a919ce8b53b9
Vicente Monfort Salvador <vmonfort@florida-uni.es>
2021-12-06 15:12:28 +0100

    Dejando en raiz php y test

---
+++ Calculadora.php
@@ -0,0 +1,20 @@
+<?php​
+
+//Clase ejemplo para probar la CI en GitHub
+//Modificando en GitHub
+class Calculadora {​
+
+    private $num1;​
+    private $num2;​
+​
+    public function __construct($a1,$a2) {​
+        $this->num1=$a1;​
+        $this->num2=$a2;​
+    }​
+    
+    public function suma(){​
+        return $this->num1+$this->num2;​
+    }​
+}​
+
+?>​
